d03599aea5da555e6b5b84caf409c34b0cf73d1a
Luke <lukeconrad.stew@gmail.com>
2022-03-31 21:33:43 -0700

    fixed webpack and service worker to let install button work

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,9 +34,9 @@ module.exports = () => {
     new WebpackPwaManifest({
       fingerprints: false,
       inject: true,
-      name: "Just Another Text Editor",
-      short_name: "JATE",
-      description: "Never lose your notes again!",
+      name: "Text Editor",
+      short_name: "",
+      description: "Take your notes here",
       background_color: "#225ca3",
       theme_color: "#225ca3",
       start_url: "/",
